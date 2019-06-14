5a25a4ebe23da08c1ce18307938da2f39e374181
Alexandr Gorchakov <alexmakeresponsive@gmail.com>
2019-06-14 11:25:09 +0600

    index.php: define constants, init IncludeComponent function, registerNamespaces

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
+
+define("LANG",          's1');
+define("SITE_ID",       's1');
+define("SITE_DIR",      '/');
+define("LANG_DIR",      '/');
+define("LANGUAGE_ID",   'ru');
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/loader.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/amr/modules/main/tools.php';
 
 \spl_autoload_register([Loader::class, 'autoLoad']);
 
 Loader::switchAutoLoad(true);
 Loader::registerNamespace('Amr\Main\Classes', 'main', 'classes' );
+Loader::registerNamespace('Amr\Main\Classes\General', 'main', 'classes/general' );
+Loader::registerNamespace('Amr\Main\Classes\Mysql', 'main', 'classes/mysql' );
 Loader::registerNamespace('Amr\Main\Lib', 'main', 'lib' );
 Loader::registerNamespace('Amr\Main\Lib\Type', 'main', 'lib/type' );
 Loader::registerNamespace('Amr\Main\Lib\Config', 'main', 'lib/config' );
@@ -14,8 +24,8 @@ Loader::registerNamespace('Amr\Main\Lib\Config', 'main', 'lib/config' );
 $appHttp         =Amr\Main\Lib\HttpApplication::getInstance();
 $appHttp        ->initializeBasicKernel();
 
-$app            =Amr\Main\Lib\Application::getInstance();
-$app           ->initializeExtendedKernel(array(
+$app             =Amr\Main\Lib\Application::getInstance();
+$app            ->initializeExtendedKernel(array(
                 	"get"      => $_GET,
                 	"post"     => $_POST,
                 	"files"    => $_FILES,
@@ -25,5 +35,16 @@ $app           ->initializeExtendedKernel(array(
                 ));
             // ->initializeContext($params);
 
-echo "<pre>";
-var_dump($app->getContext());die;
+$main           =new Amr\Main\Classes\Mysql\CMain;
+$main          ->IncludeComponent("amr:menu", "bottom_menu", array(
+                        "ROOT_MENU_TYPE" => "bottom",
+                        "MAX_LEVEL" => "1",
+                        "MENU_CACHE_TYPE" => "A",
+                        "CACHE_SELECTED_ITEMS" => "N",
+                        "MENU_CACHE_TIME" => "36000000",
+                        "MENU_CACHE_USE_GROUPS" => "Y",
+                        "MENU_CACHE_GET_VARS" => array(
+                        ),
+                    ),
+                    false
+                );
