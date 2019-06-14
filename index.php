40e14c8bf7da369421d7cb19fff78708b41cd948
Alexandr Gorchakov <alexmakeresponsive@gmail.com>
2019-06-14 13:11:01 +0600

    index.php: define SITE_TEMPLATE_ID

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ define("SITE_ID",       's1');
 define("SITE_DIR",      '/');
 define("LANG_DIR",      '/');
 define("LANGUAGE_ID",   'ru');
+define("SITE_TEMPLATE_ID",   'twbs4_1');
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/loader.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/amr/modules/main/tools.php';
@@ -38,15 +39,18 @@ $app            ->initializeExtendedKernel(array(
             // ->initializeContext($params);
 
 $main           =new Amr\Main\Classes\Mysql\CMain;
-$main          ->IncludeComponent("amr:menu", "bottom_menu", array(
-                        "ROOT_MENU_TYPE" => "bottom",
-                        "MAX_LEVEL" => "1",
-                        "MENU_CACHE_TYPE" => "A",
-                        "CACHE_SELECTED_ITEMS" => "N",
-                        "MENU_CACHE_TIME" => "36000000",
-                        "MENU_CACHE_USE_GROUPS" => "Y",
-                        "MENU_CACHE_GET_VARS" => array(
+$main          ->IncludeComponent(
+                        "amr:menu",
+                        "bottom_menu",
+                        array(
+                            "ROOT_MENU_TYPE" => "bottom",
+                            "MAX_LEVEL" => "1",
+                            "MENU_CACHE_TYPE" => "A",
+                            "CACHE_SELECTED_ITEMS" => "N",
+                            "MENU_CACHE_TIME" => "36000000",
+                            "MENU_CACHE_USE_GROUPS" => "Y",
+                            "MENU_CACHE_GET_VARS" => array(
+                            ),
                         ),
-                    ),
-                    false
+                        false   // parent component
                 );
